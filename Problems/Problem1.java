f12f2b622e250918a137ceaf1b2a36c88cffc86c
arunpalanivel2377 <arunpalanivel2377@gmail.com>
2025-03-29 22:40:22 +0530

    Modified

--- Problems/Problem1.java
+++ Problems/Problem1.java
@@ -6,15 +6,22 @@ A digit d of n divides n evenly if the remainder when n is divided by d is 0 (n 
 Digits of n should be checked individually. If a digit is 0, it should be ignored because division by 0 is undefined.
 
 Examples :
-
+<---------------------------------------------------------------------------------------------------------->
 Input: n = 12
 Output: 2
+
 Explanation: 1, 2 when both divide 12 leaves remainder 0.
+
+<---------------------------------------------------------------------------------------------------------->
 Input: n = 2446
 Output: 1
+
 Explanation: Here among 2, 4, 6 only 2 divides 2446 evenly while 4 and 6 do not.
+
+<---------------------------------------------------------------------------------------------------------->
 Input: n = 23
 Output: 0
+
 Explanation: 2 and 3, none of them divide 23 evenly.
 
  */
